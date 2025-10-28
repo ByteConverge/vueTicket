3ccf43d3085e04c5503221e85abb89234da16b58
Oyama <godstimeoyama74@gmail.com>
2025-10-28 20:52:17 +0100

    fixed case sensitivity issue

--- src/router.js
+++ src/router.js
@@ -2,19 +2,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { authUtils } from './utils/auth'
 
-// Comment out or remove Landing.vue import for now
-// import Landing from './pages/Landing.vue'
-import Login from '../pages/Login.vue'
-import Signup from '../pages/Signup.vue'
-import Dashboard from '../pages/Dashboard.vue'
-import Tickets from '../pages/Tickets.vue'
+// Use absolute paths from src directory
+import Landing from '@/Pages/Landing.vue'
+import Login from '@/Pages/Login.vue'
+import Signup from '@/Pages/Signup.vue'
+import Dashboard from '@/Pages/Dashboard.vue'
+import Tickets from '@/Pages/Tickets.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Landing',
-    // Temporarily use Login as landing page
-    component: Login
+    component: Landing
   },
   {
     path: '/login',
